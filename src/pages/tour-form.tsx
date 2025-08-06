f96c852e4c71e8735f72e779905029fdba5b8cb9
NguyenDaiHoang <teamax717@gmail.com>
2025-08-06 20:33:04 +0700

    update 06/08/2025

--- src/pages/tour-form.tsx
+++ src/pages/tour-form.tsx
@@ -1,5 +1,17 @@
 import Form from '@/modules/Mail/Form/Form';
+import VerifyEmail from '@/modules/Mail/VerifyEmail/VerifyEmail';
 import Head from 'next/head';
+const sampleData = {
+  tenNguoiDat: 'Nguyen Van A',
+  email: 'example@gmail.com',
+  soDienThoai: '0123456789',
+  soCCCD: '123456789',
+  tenTour: 'Ha Long Bay Tour',
+  ngayDi: '2025-08-10',
+  ngayVe: '2025-08-15',
+  maDonHang: 'DH001',
+  nguoiDiCung: [{ ten: 'Nguyen Van B', doiTuong: 'Bạn', soGhe: '2' }],
+};
 
 export default function TourForm() {
   return (
@@ -8,7 +20,8 @@ export default function TourForm() {
         <title>Trang đặt tour</title>
         <meta name="description" content="Chào mừng đến với VN Tourist - Trang đặt tour ." />
       </Head>
-      <Form />
+      {/* <Form /> */}
+      <VerifyEmail data={sampleData} />
     </>
   );
 }
